5ca84689c7943f126501c218738ef48258f98c5f
N Subramanian <subramanian.n2006@gmail.com>
2026-08-08 13:35:55 +0530

    Add files via upload

---
+++ barrel_shifter/DUT.vhdl
@@ -0,0 +1,26 @@
+-- A DUT entity is used to wrap your design.
+--  This example shows how you can do this for the
+--  Full-adder.
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity DUT is
+   port(input_vector: in std_logic_vector(11 downto 0);
+       	output_vector: out std_logic_vector(7 downto 0));
+end entity;
+
+architecture DutWrap of DUT is
+	-- Instantiate your own top Module component in place of ALU_1
+	
+component bs is
+port(l,b2,b1,b0,a7,a6,a5,a4,a3,a2,a1,a0: in std_logic; s7,s6,s5,s4,s3,s2,s1,s0: out std_logic);
+end component;
+
+begin
+
+   -- input/output vector element ordering is critical,
+   -- and must match the ordering in the trace file!
+   add_instance: bs port map (l => input_vector(11), b2 => input_vector(10), b1 => input_vector(9), b0 => input_vector(8), a7 => input_vector(7), a6 => input_vector(6), a5 => input_vector(5), a4 => input_vector(4), a3 => input_vector(3), a2 => input_vector(2), a1 => input_vector(1), a0 => input_vector(0), s7 => output_vector(7), s6 => output_vector(6), s5 => output_vector(5), s4 => output_vector(4), s3 => output_vector(3), s2 => output_vector(2), s1 => output_vector(1), s0 => output_vector(0));
+
+end DutWrap;
